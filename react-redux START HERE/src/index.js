2a8437c89736f1e70817b7ad30bbe984e74c28f6
huangfuin11 <huangfuin1101@yahoo.com.tw>
2019-12-29 21:17:13 +0000

    structure folders

--- react-redux START HERE/src/index.js
+++ react-redux START HERE/src/index.js
@@ -1,6 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+//Reducer stuff
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import promiseMiddleware from 'redux-promise';
+
 import App from './components/App/App';
+import reducers from './reducers';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const storeMiddleware = applyMiddleware(promiseMiddleware)(createStore);
+
+
+
+
+ReactDOM.render(
+  <Provider store={storeMiddleware(reducers)}>
+    <App />
+  </Provider>, 
+  document.getElementById('root')
+);
---
+++ react-redux START HERE/src/reducers/index.js
@@ -0,0 +1,11 @@
+import { combineReducers } from 'redux';
+import home from './home_reducer';
+import movie from './movie_reducer';
+
+const rootReducer = combineReducers({
+  home, 
+  movie
+});
+
+export default rootReducer;
+
